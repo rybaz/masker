f7fb2844b92c26a757bdefb90f2cfcdb0fac2cf5
r3s-ryan <ryan.basden@risk3sixty.com>
2020-01-05 13:33:38 -0500

    First version of masker that will create masks

--- masker.py
+++ masker.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
 
-import re
-import sys
-import os
 import argparse
 
+# Add the ability to take a file as an argument
+parser = argparse.ArgumentParser(description='List of passwords to convert to masks')
+parser.add_argument('-f', default=1, type=str)
+# Get file as variable to use later
+passFile = 'tests.txt'
+
 def main():
-    # Add the ability to take a file as an argument
-    # parser = argparse.ArgumentParser(description='List of passwords to convert to masks')
-    # parser.add_argument('-f', default=1, type=str)
-    # Get file as variable to use later
-    passFile = open('tests')
-    passFile = passFile.read()
+    # Print out new passfile
+    print("New mask list:" + "\n")
     convert(passFile)
 
 def convert(i):
-    print(i)
+
+    with open(i) as file:
+        for line in file:
+            list1 = list(line)
+            masklist = ""
+            # Remove last item in list since it will always be a newline character
+            del list1[-1]
+            for n, char in enumerate(list1):
+                if char.isupper():
+                    list1[n] = "?u"
+                elif char.islower():
+                    list1[n] = "?l"
+                elif char.isdigit():
+                    list1[n] = "?d"
+                else:
+                    list1[n] = "?s"
+            # Join broken lines back to strings
+            masklist = masklist.join(list1)
+            print(masklist)
 
 main()
