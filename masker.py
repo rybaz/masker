0fee98821f97c1ae9327beb72c3dffe4bf88c2a6
r3s-ryan <ryan.basden@risk3sixty.com>
2020-01-06 14:05:15 -0500

    Fixed file variables

--- masker.py
+++ masker.py
@@ -3,16 +3,16 @@
 import argparse
 import collections
 
-# Get file as variable to use later
+# Get files as variable to use later
 passFile = 'passwords.txt'
-outFile = 'results.txt'
+resultsFile = 'results.txt'
 
 def main():
 
     print("[*] Converting passwords to masks..." + "\n")
     convertAndSort(passFile)
     # Print out new passfile
-    print("[*] Count and frequency results printed to: " + outFile + "\n")
+    print("[*] Count and frequency results printed to: " + resultsFile + "\n")
 
 def convertAndSort(i): # Converting input list to hashcat masks and running analysis
 
@@ -34,7 +34,7 @@ def convertAndSort(i): # Converting input list to hashcat masks and running anal
                     list1[n] = "?s"
             
             # Writing masks to results.txt
-            with open('results.txt', 'a+') as outFile:
+            with open(resultsFile, 'a+') as outFile:
                 # Join broken lines back to strings
                 maskList = maskList.join(list1)
                 outFile.write(maskList + "\n")
@@ -42,4 +42,4 @@ def convertAndSort(i): # Converting input list to hashcat masks and running anal
             outFile.close()
 
 if __name__== "__main__":
-  main()
+    main()
