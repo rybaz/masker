d0ddfff4cb09375a03a4c8793c367a031637164c
r3s-ryan <ryan.basden@risk3sixty.com>
2020-01-06 13:14:06 -0500

    Regular dev commit

--- masker.py
+++ masker.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 
 import argparse
+import collections
 
 # Add the ability to take a file as an argument
 parser = argparse.ArgumentParser(description='List of passwords to convert to masks')
 parser.add_argument('-f', default=1, type=str)
 # Get file as variable to use later
-passFile = 'tests.txt'
+passFile = 'passwords.txt'
+outFile = 'results.txt'
 
 def main():
+
+    print("[*] Converting passwords to masks..." + "\n")
+    convertAndSort(passFile)
     # Print out new passfile
-    print("New mask list:" + "\n")
-    convert(passFile)
+    print("\n" + "[*] Count and frequency results printed to: " + outFile)
 
-def convert(i):
+def convertAndSort(i): # Converting input list to hashcat masks and running analysis
 
+    # Character conversion
     with open(i) as file:
         for line in file:
             list1 = list(line)
-            masklist = ""
+            maskList = ""
             # Remove last item in list since it will always be a newline character
             del list1[-1]
             for n, char in enumerate(list1):
@@ -31,7 +36,9 @@ def convert(i):
                 else:
                     list1[n] = "?s"
             # Join broken lines back to strings
-            masklist = masklist.join(list1)
-            print(masklist)
+            maskList = maskList.join(list1)
+            print(maskList)
+        
+        # Count recurring lines and print counts
 
 main()
